bcb900605bf378d6f4eff2a8eb47fc519b738716
Aleksey Konotop <alekseykonotop@gmail.com>
2019-04-03 12:57:11 +0300

    При попытке добавить через админку экземпляр телеофна. Получаю такую ошибку: no such table: phones_phone

--- databases/phone_comparison/phones/admin.py
+++ databases/phone_comparison/phones/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Phone, IPhone, Samsung, Xiaomi
 
-# Register your models here.
+
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+
+
+class IPhoneAdmin(admin.ModelAdmin):
+    pass
+
+
+class SamsungAdmin(admin.ModelAdmin):
+    pass
+
+
+class XiaomiAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(IPhone, IPhoneAdmin)
+admin.site.register(Samsung, SamsungAdmin)
+admin.site.register(Xiaomi, XiaomiAdmin)
--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Phone(models.Model):
+    brand = models.TextField()
+    model = models.TextField()
+    cost = models.IntegerField()
+    operating_system = models.TextField()
+    cpu = models.TextField()  # Процессор
+    display_size = models.FloatField()
+    pixels_per_inch = models.IntegerField()
+    duble_camera = models.BooleanField()
+    number_of_SIM_cards = models.IntegerField()
+    SIM_card_type = models.TextField()
+    moisture_protection = models.BooleanField()
+
+
+class IPhone(Phone):
+    digital_zoom = models.BooleanField()
+
+
+class Samsung(Phone):
+    A_GPS_module = models.BooleanField()
+    memory_card_support = models.TextField()
+    max_size_memory_card = models.IntegerField()
+
+
+class Xiaomi(Phone):
+    graphic_accelerator = models.TextField()
