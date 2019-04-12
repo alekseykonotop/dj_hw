078ee4d13b1ef9e76771e89fcf92777f9cb6d3b3
Aleksey Konotop <alekseykonotop@gmail.com>
2019-04-12 16:40:21 +0300

    Получаю ошибку <class 'articles.admin.ArticleAdmin'>: (admin.E103) The value of 'inlines' must be a list or tuple.

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Category, Compilation
+
+
+class CompilationInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+
+        for form in self.forms:
+            if form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    counter += 1
+        if counter == 0:
+            raise ValidationError('Укажите основной раздел')
+        if counter > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+
+
+class CompilationInline(admin.TabularInline):
+    model = Category.articles.through
+    extra = 1
+    formset = CompilationInlineFormset
 
 
 @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = (CompilationInline)
+
+
+@admin.register(Category)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -12,5 +12,28 @@ class Article(models.Model):
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
 
+
+    class Meta:
+        ordering = ['-title']
+
     def __str__(self):
-        return self.title
+        return {self.title}
+
+
+# My code
+class Category(models.Model):
+    name = models.CharField(max_length=65, verbose_name='Категория')
+    is_main = models.BooleanField(null=True, verbose_name='Основная категория')
+    articles = models.ManyToManyField(Article, related_name='categories')
+
+    def __str__(self):
+        return {self.name}
+
+
+class Compilation(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.article} {self.category}'
+
