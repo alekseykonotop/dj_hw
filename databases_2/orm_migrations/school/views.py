2432c74728929e9f7c618295ceb2140780ba86ef
Aleksey Konotop <alekseykonotop@gmail.com>
2019-04-14 21:27:04 +0300

    The first programme version is done. Its work correctly, but needs to the optimization.

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -4,5 +4,10 @@ from .models import Student
 
 
 class StudentListView(ListView):
+    template_name = 'school/student_list.html'
     model = Student
     ordering = 'group'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs
