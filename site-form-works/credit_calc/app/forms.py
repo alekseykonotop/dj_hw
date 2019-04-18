bd3c1276f7645947db2f84630dddaed22d0855b2
Aleksey Konotop <alekseykonotop@gmail.com>
2019-04-18 22:27:50 +0300

    Частично реализован калькулятор. Нужно убрать вывод ошибок при первой загрузки

--- site-form-works/credit_calc/app/forms.py
+++ site-form-works/credit_calc/app/forms.py
@@ -9,9 +9,25 @@ class CalcForm(forms.Form):
     def clean_initial_fee(self):
         # валидация одного поля, функция начинающаяся на `clean_` + имя поля
         initial_fee = self.cleaned_data.get('initial_fee')
+        print(f'initial_fee ==> {initial_fee}')
         if not initial_fee or initial_fee < 0:
             raise forms.ValidationError("Стоимость товара не может быть отрицательной")
         return initial_fee
+
+    def clean_rate(self):
+        rate = self.cleaned_data.get('rate')
+        print(f'rate ==> {rate}')
+        if not rate or int(rate) < 0 or float(rate) < 0:
+            raise forms.ValidationError("Процент не может быть отрицательным")
+        print(f'rate 2 ==> {rate}')
+        return rate
+
+    def clean_months_count(self):
+        months_count = self.cleaned_data.get('months_count')
+        print(f'months_count ==> {months_count}')
+        if not months_count or months_count < 1:
+            raise forms.ValidationError("Колличество месяцев должно быть больше 1")
+        return months_count
 
     def clean(self):
         # общая функция валидации
--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -1,8 +1,38 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 from .forms import CalcForm
 
 
 class CalcView(TemplateView):
     template_name = "app/calc.html"
+
+
+    def get(self, request, *args, **kwargs):
+        form = CalcForm(request.GET)
+        if not form.is_valid():
+            if not form.is_bound:
+                print(f'start branch not form.is_bound')
+                print(f'form.is_bound ==> {form.is_bound}')
+                return render(request, self.template_name, {'form': CalcForm()})
+
+            print(f'start branch not form.is_valid()')
+            print(f'errors ==> {form.errors}')
+            return render(request, self.template_name, {'form': CalcForm(request.GET)})
+
+        print(f'Form is valid')
+        initial_fee = form.cleaned_data['initial_fee']
+        rate = form.cleaned_data['rate']
+        months_count = form.cleaned_data['months_count']
+        common_result = round(initial_fee + (initial_fee * int(rate) / 100))
+        result = round(common_result / months_count, 2)
+
+        context = {
+            'form': form,
+            'common_result': common_result,
+            'result': result,
+        }
+
+        return render(request, self.template_name, context)
+
 
