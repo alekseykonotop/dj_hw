6c8b40421cfc9c776cdfb8b8ee8a7437f0a21d49
Aleksey Konotop <alekseykonotop@gmail.com>
2019-04-20 14:06:43 +0300

    Calculator is done

--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -1,38 +1,33 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-
 from .forms import CalcForm
 
 
 class CalcView(TemplateView):
     template_name = "app/calc.html"
 
-
     def get(self, request, *args, **kwargs):
+        if not request.GET:
+            return render(request, self.template_name, {'form': CalcForm()})
+
         form = CalcForm(request.GET)
+        context = {
+            'form': form
+        }
         if not form.is_valid():
-            if not form.is_bound:
-                print(f'start branch not form.is_bound')
-                print(f'form.is_bound ==> {form.is_bound}')
-                return render(request, self.template_name, {'form': CalcForm()})
+            return render(request, self.template_name, context)
 
-            print(f'start branch not form.is_valid()')
-            print(f'errors ==> {form.errors}')
-            return render(request, self.template_name, {'form': CalcForm(request.GET)})
-
-        print(f'Form is valid')
         initial_fee = form.cleaned_data['initial_fee']
         rate = form.cleaned_data['rate']
         months_count = form.cleaned_data['months_count']
-        common_result = round(initial_fee + (initial_fee * int(rate) / 100))
-        result = round(common_result / months_count, 2)
 
-        context = {
-            'form': form,
-            'common_result': common_result,
-            'result': result,
-        }
+        context['common_result'] = round(initial_fee + (initial_fee * int(rate) / 100))
+        context['result'] = round(context['common_result'] / months_count, 2)
 
         return render(request, self.template_name, context)
+
+
+
+
 
 
