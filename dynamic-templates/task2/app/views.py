864da5803dd38cceb47652c78de260f8b7a01fff
Aleksey Konotop <alekseykonotop@gmail.com>
2019-05-14 23:02:52 +0300

    Внес правки согласно замечаниям

--- dynamic-templates/task2/app/views.py
+++ dynamic-templates/task2/app/views.py
@@ -3,48 +3,33 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'app/includes/include_home.html'
+    template_name = 'app/home.html'
 
     def get(self, request, *args, **kwargs):
 
-        context = {
-            'path_to_page': 'app/includes/include_home.html',
-            'items': ''
-        }
-
-        return render(request, self.template_name, context)
+        return render(request, self.template_name)
 
 
 class AboutView(TemplateView):
-    template_name = 'app/includes/include_about.html'
+    template_name = 'app/about.html'
 
     def get(self, request, *args, **kwargs):
 
-        context = {
-            'path_to_page': 'app/includes/include_about.html',
-            'items': ''
-        }
-
-        return render(request, self.template_name, context)
+        return render(request, self.template_name)
 
 
 
 
 class ContactsView(TemplateView):
-    template_name = 'app/includes/include_contacts.html'
+    template_name = 'app/contacts.html'
 
     def get(self, request, *args, **kwargs):
 
-        context = {
-            'path_to_page': 'app/includes/include_contacts.html',
-            'items': ''
-        }
-
-        return render(request, self.template_name, context)
+        return render(request, self.template_name)
 
 
 class ExamplesView(TemplateView):
-    template_name = 'app/includes/include_examples.html'
+    template_name = 'app/examples.html'
 
     def get(self, request, *args, **kwargs):
         items = [{
@@ -61,8 +46,8 @@ class ExamplesView(TemplateView):
             'img': 'imac.jpg'
         }]
         context = {
-            'path_to_page': 'app/includes/include_examples.html',
             'items': items
         }
+
         return render(request, self.template_name,
                       context)
