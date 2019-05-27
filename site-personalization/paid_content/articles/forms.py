1e3c0bfa996bc11b853bfcf101ac14e4d7a81b77
Aleksey Konotop <alekseykonotop@gmail.com>
2019-05-27 22:28:44 +0300

    Minor edits in Paid content.

--- site-personalization/paid_content/articles/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-# Надо проверить - возможно этот класс вообще не нужен
-class SubscribeProfileForm(forms.ModelForm):
-
-    class Meta(object):
-        model = Profile
-        exclude = ('id', )
